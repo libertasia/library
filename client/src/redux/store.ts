9ef97c975100ac337130fa36adca888ffcaf9311
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-06-14 11:10:05 +0300

    Need devtools in prod for now

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,11 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevToolsDevelopmentOnly } from '@redux-devtools/extension'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import rootReducer from '../redux/reducers'
 
 const store = createStore(
   rootReducer,
-  composeWithDevToolsDevelopmentOnly(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store
